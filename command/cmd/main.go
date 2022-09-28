745923ab66e764e2ec29a1c12aa49a5b53fbd5ca
Aoi Kondo <agotadmidori@gmail.com>
2022-09-28 09:49:40 +0900

    refactoring (about functions)
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- command/cmd/main.go
+++ command/cmd/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
-	"server/command/pkg"
+	""
 )
 
 func main() {
@@ -19,15 +19,7 @@ func main() {
 		pkg.Start_server()
 		
 	case "help":
-		fmt.Println("'Judgelight' is competitive-programming providing service.")
-		fmt.Println("")
-		fmt.Println("Usage:")
-		fmt.Println("jl <command> [arguments]")
-		fmt.Println("")
-		fmt.Println("The commands are:")
-		fmt.Println("start")
-		fmt.Println("help")
-		os.Exit(0)
+		
 	default:
 		os.Exit(1)
 	}
