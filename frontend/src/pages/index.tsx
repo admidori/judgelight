61c10ef4d9d23135f2d04556e3c50a51ec66a8da
Aoi Kondo <agotadmidori@gmail.com>
2022-09-28 22:27:08 +0900

    Add test POST method.
    I'll make textbox format soon
    
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 import Header from "../components/header/header";
 import Footer from '../components/footer/footer';
 import Menubar from '../components/menubar/menubar';
+
+var params = new URLSearchParams();
+params.append("program", "itisdata")
+const url = axios.post("http://backend:8080/post", params)
+.then(function (response) {
+    console.log(response);
+})
+.catch(function (error) {
+    console.log(error);
+});
 
 class Index extends React.Component {
     render() {
