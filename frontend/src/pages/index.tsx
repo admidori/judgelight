567b6182b0613d8cd5278f282e8be852dc558dff
Aoi Kondo <agotadmidori@gmail.com>
2022-09-29 21:16:59 +0900

    Complete writing about code assigned
    When users click "submit" button , it assgin value
    
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
+
 import Header from "../components/common/header/header";
 import Footer from '../components/common/footer/footer';
 import Menubar from '../components/common/menubar/menubar';
-import Programbox from '../components/programbox/programbox';
+
+import Editor from 'react-simple-code-editor';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
 
 var params = new URLSearchParams();
 params.append("program", "itisdata")
@@ -15,12 +20,33 @@ const url = axios.post("http://backend:8080/post", params)
     console.log(error);
 });
 
+const code = `function add(a, b) {
+    return a + b;
+}
+`;
+
+const handleClick = (code) => {
+    console.log(code)
+}
+
 class Index extends React.Component {
+    state = { code }
     render() {
         return (
             <div>
             <Menubar />
-            <Programbox />
+            <Editor
+                value={this.state.code}
+                onValueChange={code => this.setState({ code })}
+                highlight={code => highlight(code, languages.js)}
+                padding={10}
+                style={{
+                    fontFamily: '"Fira code", "Fira Mono", monospace',
+                    fontSize: 12,
+                }}
+            />
+
+            <button onClick={() => handleClick(this.state)}>Submit</button>
             <Footer />
             </div>
         );
