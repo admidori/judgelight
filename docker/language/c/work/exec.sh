d976d7aaf889be781117922a23a774357d6fc49b
Aoi Kondo <agotadmidori@gmail.com>
2023-07-03 20:15:26 +0900

    Finally delete temporary files.

--- docker/language/c/work/exec.sh
+++ docker/language/c/work/exec.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
+
 COMPILECODE="${SUBMITFILENAME}.${SUBMITLANGUAGE}"
 
 cd src
 gcc "${COMPILECODE}" -o "${SUBMITFILENAME}"
 RET=$?
-touch test.c
+
+function delete_files (){
+    rm "./${SUBMITFILENAME}" \
+        "${SUBMITFILENAME}.txt" \
+        "./answerset/${SUBMITFILENAME}.txt" \
+        "${COMPILECODE}"
+}
 
 if [ $RET = 0 ]; then
     "./${SUBMITFILENAME}" > "${SUBMITFILENAME}.txt"
 else
     echo "CE"
     # Return code 2 if CE
+    rm "${COMPILECODE}"
     exit 2
 fi
 
 if ! diff "${SUBMITFILENAME}.txt" "./answerset/${SUBMITFILENAME}.txt"; then
     echo "WA"
+    delete_files
+
     # Return code 1 if WA
     exit 1
 else
     echo "AC"
+    delete_files
+
     # Return code 0 if AC
     exit 0
 fi
