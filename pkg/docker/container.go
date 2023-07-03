2e99d69b1e0ee3200ae277236cac097890407b26
Aoi Kondo <agotadmidori@gmail.com>
2023-07-03 19:46:43 +0900

    Developed about status code

--- pkg/docker/container.go
+++ pkg/docker/container.go
@@ -8,6 +8,7 @@ import (
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/mount"
 	"github.com/docker/docker/client"
+	"github.com/rp-agota/judgelight/pkg/controller/submit"
 )
 
 func ContainerCreateAndStart(filename string, language string) {
@@ -35,7 +36,7 @@ func ContainerCreateAndStart(filename string, language string) {
 				{
 					Type:   mount.TypeBind,
 					Source: "/Users/agota/judgelight/docker/language/c/",
-					Target: "/work",
+					Target: "/workspace",
 				},
 			},
 		}, nil, nil, "")
@@ -46,9 +47,7 @@ func ContainerCreateAndStart(filename string, language string) {
 	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
 		panic(err)
 	}
-	//Nexttask:
-	//	Copy exec.sh to Docker container and expect return code.
-	//	When copy script , refer to web page
+
 	exitCodeCh, errch := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
 	select {
 	case err := <-errch:
@@ -59,19 +58,22 @@ func ContainerCreateAndStart(filename string, language string) {
 		//WA
 		if exitCode.StatusCode == 1 {
 			fmt.Println("WA")
+			submit.SendWAsignal()
 		}
 		//CE
 		if exitCode.StatusCode == 2 {
 			fmt.Println("CE")
+			submit.SendCEsignal()
 		}
 		//AC
 		if exitCode.StatusCode == 0 {
-			fmt.Println("goAC")
+			fmt.Println("AC")
+			submit.SendACsignal()
 		}
 	}
 
 	err = cli.ContainerRemove(ctx, resp.ID, types.ContainerRemoveOptions{})
 	if err != nil {
-	panic(err)
+		panic(err)
 	}
 }
