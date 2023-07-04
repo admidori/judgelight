042fd5fef6c9c8bcfbd0b96f440cebb9abb7e7ee
Aoi Kondo <agotadmidori@gmail.com>
2023-07-04 22:01:36 +0900

    Add subcommands.

--- pkg/command/start.go
+++ pkg/command/start.go
@@ -5,38 +5,19 @@ Copyright © 2023 Aoi Kondo <agotadmidori@gmail.com>
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
-// startCmd represents the start command
 var startCmd = &cobra.Command{
 	Use:   "start",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-
+	Short: "Run judgelight programs",
+	Long: `Run judgelight programs on docker container.
+	After excute command, Run docker compose and start nginx and api containers.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// If called start command, excute below.
-		fmt.Println("start called")
+		// Exec docker compose up
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(startCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
---
+++ pkg/etc/version.go
@@ -0,0 +1,5 @@
+package etc
+
+var VERSION_JUDGELIGHT = "0.1"
+var DEVELOPMENT_CODE = "beta"
+var RELEASE_DATE = ""
