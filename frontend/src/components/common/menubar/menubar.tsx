458f60f9ae23dae4ea085df157db1bbbd59ccd0e
Aoi Kondo <agotadmidori@gmail.com>
2023-07-12 19:22:02 +0900

    Fix typo

--- frontend/src/components/common/menubar/menubar.tsx
+++ frontend/src/components/common/menubar/menubar.tsx
@@ -5,7 +5,7 @@ export default function Menubar(){
     return (
     <nav style={{ background: "#666" }}>
         <ul style={{ display: 'flex', listStyle: 'none' }}>
-            <li style={{ margin: 10 }}><Link href="/index" style={{ color: "#fff", textDecoration: 'none' }}>Home</Link></li>
+            <li style={{ margin: 10 }}><Link href="/" style={{ color: "#fff", textDecoration: 'none' }}>Home</Link></li>
             <li style={{ margin: 10 }}><Link href="/result" style={{ color: "#fff", textDecoration: 'none' }}>Result</Link></li>
         </ul>
     </nav>
