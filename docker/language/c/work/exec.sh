bf36ddff10d3180435c512accc3399c70a87443c
Aoi Kondo <agotadmidori@gmail.com>
2023-07-13 20:56:38 +0900

    Update exec.sh Rewrite code for multiple testcase judge

--- docker/language/c/work/exec.sh
+++ docker/language/c/work/exec.sh
@@ -2,9 +2,16 @@
 
 COMPILECODE="${SUBMITFILENAME}.${SUBMITLANGUAGE}"
 
-cd src
-gcc "${COMPILECODE}" -o "${SUBMITFILENAME}"
-RET=$?
+function check_casefile_num (){
+    local cnt=0
+
+    for file in "${1}/*"
+    do
+        cnt=${cnt} + 1
+    done
+
+    return ${cnt}
+}
 
 function delete_files (){
     rm "./${SUBMITFILENAME}" \
@@ -12,25 +19,48 @@ function delete_files (){
         "${COMPILECODE}"
 }
 
-if [ $RET = 0 ]; then
-    "./${SUBMITFILENAME}" > "${SUBMITFILENAME}.txt"
-else
-    echo "CE"
-    # Return code 2 if CE
-    rm "${COMPILECODE}"
-    exit 2
-fi
+function check_files (){
+    for cnt in ${2}
+    do
+        "./${PROBLEMID}/${1}/input.txt" > "./${SUBMITFILENAME}" > "${SUBMITFILENAME}.txt"
+        if ! diff "${SUBMITFILENAME}.txt" "./${PROBLEMID}/${1}/output.txt"; then
+            echo "WA"
+            # Return code 1 if WA
+            return 1
+        fi
+    done
 
-if ! diff "${SUBMITFILENAME}.txt" "./answerset/${PROBLEMID}.txt"; then
-    echo "WA"
-    delete_files
-
-    # Return code 1 if WA
-    exit 1
-else
     echo "AC"
-    delete_files
-
     # Return code 0 if AC
+    return 0
+}
+
+function compile (){
+    gcc "${COMPILECODE}" -o "${SUBMITFILENAME}"
+    RET=$?
+
+    if [ $RET != 0 ]; then
+        echo "CE"
+        # Return code 2 if CE
+        rm "${COMPILECODE}"
+        exit 2
+    fi
+}
+
+cd src
+compile
+
+CASEFILE_NUM_EXAMPLE=`check_casefile_num examplecase`
+CASEFILE_NUM_TEST=`check_casefile_num testcase`
+
+RESULT_EXAMPLE=`check_files examplecase CASEFILE_NUM_EXAMPLE`
+RESULT_TEST=`check_files testcase CASEFILE_NUM_TEST`
+
+delete_files
+
+if [ $RESULT_EXAMPLE = 0 && $RESULT_TEST = 0 ]; then
+    echo "Last Result -> AC"
     exit 0
-fi
+else
+    echo "Last Result -> WA"
+    exit 1
