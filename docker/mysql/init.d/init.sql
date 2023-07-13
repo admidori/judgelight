a4054105e633c103881d54cf02c39b194e42d9f1
Aoi Kondo <agotadmidori@gmail.com>
2023-07-13 20:39:32 +0900

    Update SQL

--- docker/mysql/init.d/init.sql
+++ docker/mysql/init.d/init.sql
@@ -16,4 +16,15 @@ CREATE TABLE JUDGELIGHT.RESULT(
     FOREIGN KEY (ID) REFERENCES USER(ID),
 );
 
+# Define problem table
+# Todo: define array
+CREATE TABLE JUDGELIGHT.PROBLEM{
+    PROBLEM_ID varchar(5) NOT NULL,
+    PROBLEM varchar(1000) NOT NULL,
+    TESTCASE_NUM int NOT NULL,
+    ANSWER varchar(1000) NOT NULL,
+    INPUT varchar(1000),
+    PRIMARY KEY (PROBLEM_ID),
+};
+
 # Future: Problem and answer dataset are inserted to MySQL DB.
