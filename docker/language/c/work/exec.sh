483b1b95c6d0b1f86a178ac2beecd026cb3e6c95
Aoi Kondo <agotadmidori@gmail.com>
2023-07-14 23:26:02 +0900

    Modify files Check excuting docker container

--- docker/language/c/work/exec.sh
+++ docker/language/c/work/exec.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
+#PROBLEMID="2"
+#SUBMITFILENAME="1"
+#SUBMITLANGUAGE="c"
 
 COMPILECODE="${SUBMITFILENAME}.${SUBMITLANGUAGE}"
 
 function check_casefile_num (){
-    local cnt=0
-
-    for file in "${1}/*"
-    do
-        cnt=${cnt} + 1
-    done
-
-    return ${cnt}
+    local rtn=$(ls -l "./case/${PROBLEMID}/${1}/" | grep ^d | wc -l)
+    return ${rtn}
 }
 
 function delete_files (){
-    rm "./${SUBMITFILENAME}" \
-        "${SUBMITFILENAME}.txt" \
-        "${COMPILECODE}"
+    rm  "./${SUBMITFILENAME}.out" \
+        "./${COMPILECODE}" \
+        "./${SUBMITFILENAME}.txt"
 }
 
 function check_files (){
-    for cnt in ${2}
+    local cnt=0
+
+    for cnt in `seq 1 ${2}`
     do
-        "./${PROBLEMID}/${1}/input.txt" > "./${SUBMITFILENAME}" > "${SUBMITFILENAME}.txt"
-        if ! diff "${SUBMITFILENAME}.txt" "./${PROBLEMID}/${1}/output.txt"; then
+        "./${SUBMITFILENAME}.out" < "./case/${PROBLEMID}/${1}/${cnt}/input.txt" > "./${SUBMITFILENAME}.txt"
+        if ! diff "./${SUBMITFILENAME}.txt" "./case/${PROBLEMID}/${1}/${cnt}/output.txt"; then
             echo "WA"
             # Return code 1 if WA
             return 1
@@ -35,32 +34,34 @@ function check_files (){
     return 0
 }
 
-function compile (){
-    gcc "${COMPILECODE}" -o "${SUBMITFILENAME}"
-    RET=$?
-
-    if [ $RET != 0 ]; then
-        echo "CE"
-        # Return code 2 if CE
-        rm "${COMPILECODE}"
-        exit 2
-    fi
-}
-
+# Compile a submit code
 cd src
-compile
+gcc -o "${SUBMITFILENAME}.out" "${COMPILECODE}" 
+RET=$?
 
-CASEFILE_NUM_EXAMPLE=`check_casefile_num examplecase`
-CASEFILE_NUM_TEST=`check_casefile_num testcase`
+if [ $RET != 0 ]; then
+    echo "CE"
+    # Return code 2 if CE
+    rm "${COMPILECODE}"
+    exit 2
+fi
 
-RESULT_EXAMPLE=`check_files examplecase CASEFILE_NUM_EXAMPLE`
-RESULT_TEST=`check_files testcase CASEFILE_NUM_TEST`
+check_casefile_num "examplecase"
+CASEFILE_NUM_EXAMPLE=$?
+check_casefile_num "testcase"
+CASEFILE_NUM_TEST=$?
+
+check_files "examplecase" ${CASEFILE_NUM_EXAMPLE}
+RESULT_EXAMPLE=$?
+check_files "testcase" ${CASEFILE_NUM_TEST}
+RESULT_TEST=$?
 
 delete_files
 
-if [ $RESULT_EXAMPLE = 0 && $RESULT_TEST = 0 ]; then
+if [ ${RESULT_EXAMPLE} = 0 ] && [ ${RESULT_TEST} = 0 ]; then
     echo "Last Result -> AC"
     exit 0
 else
     echo "Last Result -> WA"
     exit 1
+fi
