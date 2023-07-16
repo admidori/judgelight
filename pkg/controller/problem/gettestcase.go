848256d7f38bf3573a92b472bac3132be9c49f01
Aoi Kondo <agotadmidori@gmail.com>
2023-07-16 13:24:41 +0900

    Complete writing function Function about example testcase

---
+++ pkg/controller/problem/gettestcase.go
@@ -0,0 +1,40 @@
+package problem
+
+import (
+	"io/ioutil"
+	"os"
+	"strconv"
+)
+
+func GetTestcaseInput(problemNum string, num int) string {
+	dirPath := "../../settings/case/" + problemNum + "/examplecase/" + strconv.Itoa(num) + "/input.txt"
+	file, err := ioutil.ReadFile(dirPath)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(file)
+}
+
+func GetTestcaseOutput(problemNum string, num int) string {
+	dirPath := "../../settings/case/" + problemNum + "/examplecase/" + strconv.Itoa(num) + "/output.txt"
+	file, err := ioutil.ReadFile(dirPath)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(file)
+}
+
+func CheckExampleNum(num string) int {
+	var cnt = 0
+
+	for {
+		dirPath := "../../settings/case/" + num + "/examplecase/" + strconv.Itoa(cnt+1)
+		if f, err := os.Stat(dirPath); os.IsNotExist(err) || !f.IsDir() {
+			return cnt
+		} else {
+			cnt++
+		}
+	}
+}
