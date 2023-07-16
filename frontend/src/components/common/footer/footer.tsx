e8533f17d1ced7822b140454cf1929adcf1a0c18
Aoi Kondo <agotadmidori@gmail.com>
2023-07-16 21:18:44 +0900

    Delete debug log about footer

--- frontend/src/components/common/footer/footer.tsx
+++ frontend/src/components/common/footer/footer.tsx
@@ -3,7 +3,6 @@ import React from "react";
 export default function Footer(){
     return (
         <footer style={{ height: 20, background: "#666", color: "#fff" }}>
-            <h6 style={{textAlign: "center"}}>- Copyright(c) 2023 Aoi Kondo All Rights Reserved. -</h6>
         </footer>
     )
 }
