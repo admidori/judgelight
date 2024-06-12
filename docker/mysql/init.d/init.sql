f86d68e70c0ce393a00b89542b7ef41682b11bbe
Aoi Kondo <agotadmidori@gmail.com>
2024-06-13 08:32:50 +0900

    Update scheme
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- docker/mysql/init.d/init.sql
+++ docker/mysql/init.d/init.sql
@@ -2,29 +2,23 @@ CREATE DATABASE JUDGELIGHT;
 
 # Defile user table
 CREATE TABLE JUDGELIGHT.USER(
-    ID varchar(5) NOT NULL,
+    STUDENT_ID varchar(5) NOT NULL,
     PASSWORD varchar(10) NOT NULL,
     PRIMARY KEY (ID),
 );
 
 # Defile result table
 CREATE TABLE JUDGELIGHT.RESULT(
-    ID varchar(5) NOT NULL,
-    PROBLEM_ID varchar(5),
-    SUBMIT_ID varchar(5),
-    RESULT INT,
-    FOREIGN KEY (ID) REFERENCES USER(ID),
+    STUDENT_ID varchar(5) NOT NULL,
+    RESULT JSON NOT NULL,
+    FOREIGN KEY (STUDENT_ID) REFERENCES USER(STUDENT_ID),
 );
 
 # Define problem table
-# Todo: define array
 CREATE TABLE JUDGELIGHT.PROBLEM{
     PROBLEM_ID varchar(5) NOT NULL,
     PROBLEM varchar(1000) NOT NULL,
-    TESTCASE_NUM int NOT NULL,
-    ANSWER varchar(1000) NOT NULL,
-    INPUT varchar(1000),
+    TEST_CASE JSON NOT NULL,
+    SECRET_CASE JSON NOT NULL,
     PRIMARY KEY (PROBLEM_ID),
 };
-
-# Future: Problem and answer dataset are inserted to MySQL DB.
