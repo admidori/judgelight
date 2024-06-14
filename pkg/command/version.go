188c9014acbdc2a607a892922039ae5093f8f586
Aoi Kondo <agotadmidori@gmail.com>
2024-06-14 15:32:08 +0900

    Update license infomation
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- pkg/command/version.go
+++ pkg/command/version.go
@@ -7,19 +7,19 @@ package cmd
 import (
 	"fmt"
 
-	"github.com/rp-agota/judgelight/pkg/etc"
+	"github.com/admidori/judgelight/pkg/etc"
 	"github.com/spf13/cobra"
 )
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Show the judgelight version and copyright information",
-	Long: ``,
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("judgelight version -- " + etc.VERSION_JUDGELIGHT + " (" + etc.DEVELOPMENT_CODE + ")")
 		fmt.Println("Release: " + etc.RELEASE_DATE)
-		fmt.Println("Copyright (c) 2023 Aoi Kondo (rp-agota) All Rights Reserved.")
-		fmt.Println("Github Repository: https://github.com/rp-agota/judgelight")
+		fmt.Println("Copyright (c) 2023-2024 Aoi Kondo (admidori) All Rights Reserved.")
+		fmt.Println("Github Repository: https://github.com/admidori/judgelight")
 		fmt.Println("Contact author: agotadmidori@gmail.com")
 		fmt.Println("This project is licensed under the MIT License, see the LICENSE file for details.")
 	},
