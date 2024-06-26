aad22c38edda7233dbb92401d3ad0c324618d9ba
Aoi Kondo <agotadmidori@gmail.com>
2024-06-26 16:31:26 +0900

    Create login form
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- frontend/src/components/login/login.tsx
+++ frontend/src/components/login/login.tsx
@@ -1,39 +1,118 @@
+import axios from "axios";
 import React from "react";
 
-var flag = false
-export var loginStateId = ""
-
 export default function Login(){
+    const [loginFlag, setLoginFlag] = React.useState("0")
     const [id, setId] = React.useState(null)
-    const [registerId, setRegisterId] = React.useState(null)
+    const [password, setPassword] = React.useState(null)
 
     React.useEffect(() => {
-        localStorage.setItem('value', JSON.stringify(registerId));
-      }, [registerId]);
+        const storedLoginFlag = localStorage.getItem("loginFlag");
+        const storedId = localStorage.getItem("id");
+        const storedPassword = localStorage.getItem("password");
+        if (storedLoginFlag && storedId && storedPassword) {
+            setLoginFlag(storedLoginFlag);
+            setId(storedId);
+            setPassword(storedPassword)
+        }
+    }, []);
+
+    const handleClickLogout = () => {
+        localStorage.setItem("loginFlag", "0")
+        localStorage.setItem("id", null)
+        localStorage.setItem("password", null)
+        setLoginFlag("0")
+        setId(null)
+        setPassword(null)
+
+        localStorage.clear();
+        console.log(localStorage)
+        alert("Logout Successfull")
+        window.location.reload();
+    }
 
     const handleClick = () => {
-        setRegisterId(id)
-        loginStateId = id
-        flag = true
+        const sendJsonData = JSON.stringify(
+            {
+                "student_id": id,
+                "password": password
+            }
+        );
+
+        axios.post('http://localhost:8080/register/login',sendJsonData)
+        .then(function(response){
+            const responseJsonData = JSON.parse(JSON.stringify(response))
+            console.log(responseJsonData.data)
+            if (responseJsonData.data.status == "success"){
+                alert("Login Successfull")
+                setLoginFlag("1")
+                localStorage.setItem("loginFlag", "1")
+                localStorage.setItem("id", id)
+                localStorage.setItem("password", password)
+            }
+            else{
+                alert("Login Failed")
+            }
+        })
+        .catch(function(error){
+            console.log(error)
+        })
     }
 
     return (
     <div>
-        <label>学籍番号</label>
         {
         (function () {
-            if (flag == true) {
+            if (loginFlag == "1") {
                 return (
                     <input value={id} readOnly></input>
                 );
             }else{
                 return(
-                    <input value={id} onChange={(event) => setId(event.target.value)}></input>
+                    <input value={id} placeholder="学籍番号" onChange={(event) => setId(event.target.value)}></input>
                 );
             }
         }())
         }
-    <button onClick={handleClick}>Register</button>
+        {
+        (function () {
+            if (loginFlag == "1") {
+                return (
+                    <input value={password} type="password" readOnly></input>
+                );
+            }else{
+                return(
+                    <input value={password} type="password" placeholder="パスワード" onChange={(event) => setPassword(event.target.value)}></input>
+                );
+            }
+        }())
+        }
+        {(
+            function(){
+                if (loginFlag == "1") {
+                    return (
+                        <button disabled onClick={handleClick}>Login</button>
+                    );
+                }else{
+                    return(
+                        <button onClick={handleClick}>Login</button>
+                    );
+                }
+            }
+        )()}
+                {(
+            function(){
+                if (loginFlag == "1") {
+                    return (
+                        <button onClick={handleClickLogout}>Logout</button>
+                    );
+                }else{
+                    return(
+                        <button disabled onClick={handleClickLogout}>Logout</button>
+                    );
+                }
+            }
+        )()}
     </div>
     );
 }
