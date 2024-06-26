7cd0882b08a360ef3ecb1e254c9bd3231414bc11
Aoi Kondo <agotadmidori@gmail.com>
2024-06-26 11:06:57 +0900

    Update header
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- frontend/src/components/common/header/header.tsx
+++ frontend/src/components/common/header/header.tsx
@@ -1,9 +1,43 @@
+import { AppBar, Container, Toolbar, Typography, Box , Button, ButtonGroup } from "@material-ui/core";
 import React from "react";
+import Link from "next/link";
 
 export default function Header() {
     return (
-        <header style={{ height: 50, background: "#666" }}>
-            <h1>Judgelight -- Version 0.1 - Beta --</h1>
-        </header>
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Box sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
+                    }}>
+                        <Box sx={{
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}>
+                            <ButtonGroup>
+                                    <Link href="/problem">
+                                    <Button
+                                        style={{ margin: '10px', color: 'white', display: 'block' , borderColor: 'white'}}
+                                    >
+                                        PROBLEMSET
+                                    </Button>
+                                    </Link>
+                                    <Link href="/result">
+                                    <Button
+                                        style={{ margin: '10px', color: 'white', display: 'block' , borderColor: 'white'}}
+                                    >
+                                        RESULT
+                                    </Button>
+                                    </Link>
+                            </ButtonGroup>
+                        </Box>
+                    </Box>
+
+                </Toolbar>
+            </Container>
+        </AppBar>
     )
 }
