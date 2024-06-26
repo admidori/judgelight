8a805deb2947a7b8892d9e616e388362668b1954
Aoi Kondo <agotadmidori@gmail.com>
2024-06-26 11:07:34 +0900

    Update footer
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- frontend/src/components/common/footer/footer.tsx
+++ frontend/src/components/common/footer/footer.tsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function Footer(){
     return (
         <footer style={{ height: 20, background: "#666", color: "#fff" }}>
+            <p style={{ textAlign: "center" }}>Judgelight v1.0 (Beta)</p>
         </footer>
     )
 }
