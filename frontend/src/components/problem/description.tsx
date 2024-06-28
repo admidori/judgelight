5493de1f1db9a75953730b97870541abdbe00e97
Aoi Kondo <agotadmidori@gmail.com>
2024-06-28 17:49:50 +0900

    Create function about problem of  description appearance
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

---
+++ frontend/src/components/problem/description.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { AppBar, Box, Toolbar } from "@material-ui/core";
+
+export default function Description(props){
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <p style={{fontSize: 12}}>{props.problemDescription}</p>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
