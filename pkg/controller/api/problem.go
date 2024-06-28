75c9a50fb195c9b546c86f2a3e21262ffe6551e4
Aoi Kondo <agotadmidori@gmail.com>
2024-06-29 08:51:25 +0900

    Create API
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- pkg/controller/api/problem.go
+++ pkg/controller/api/problem.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/admidori/judgelight/pkg/controller/database"
 	"github.com/gin-gonic/gin"
@@ -10,25 +11,83 @@ import (
 func GetProblemInfo(c *gin.Context) {
 	problem := database.ReadProblem()
 
-	param := c.Query("paramater")
+	num := c.Query("problemNumber")
+	p, _ := strconv.Atoi(num)
+	p--
+
+	param := c.Query("parameter")
+
 	if param == "NumberOfProblem" {
 		c.JSON(http.StatusOK, gin.H{
-			"paramater": len(problem),
+			"parameter": len(problem),
 		})
-	} else {
-		for _, prob := range problem {
-			m := StructToMap(&prob)
-			res := m[param]
-			c.JSON(http.StatusOK, gin.H{
-				"paramater": res,
-			})
+	}
+	if param == "Title" {
+		c.JSON(http.StatusOK, gin.H{
+			"title":         problem[p].ProblemTitle,
+			"problemNumber": problem[p].ProblemNum,
+		})
+	}
+	if param == "Description" {
+		c.JSON(http.StatusOK, gin.H{
+			"description": problem[p].ProblemDescription,
+		})
+	}
+	if param == "Appendix" {
+		c.JSON(http.StatusOK, gin.H{
+			"score":       problem[p].ProblemScore,
+			"limitTime":   problem[p].ProblemLimitTime,
+			"limitMemory": problem[p].ProblemLimitMemory,
+		})
+	}
+	if param == "Limitation" {
+		c.JSON(http.StatusOK, gin.H{
+			"input":  problem[p].ProblemLimitationInput,
+			"output": problem[p].ProblemLimitationOutput,
+		})
+	}
+	if param == "CaseTotalNumber" {
+		c.JSON(http.StatusOK, gin.H{
+			"totalNumber": len(problem[p].TestCase),
+		})
+	}
+	if param == "TestCase" {
+		total := len(problem[p].TestCase)
+		TestCaseInput := make([]string, total)
+		TestCaseOutput := make([]string, total)
+
+		for i := 0; i < total; i++ {
+			TestCaseInput[i] = problem[p].TestCase[i].Input
+			TestCaseOutput[i] = problem[p].SecretCase[i].Output
 		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"testCaseInputData":  TestCaseInput,
+			"testCaseOutputData": TestCaseOutput,
+		})
 	}
 }
 
 func SendProblem(c *gin.Context) {
-	c.Query("paramater")
+	problem := database.ReadProblem()
+
+	param := c.Query("ProblemNumber")
+	p, _ := strconv.Atoi(param)
+	p--
+	total := len(problem[p].TestCase)
+
+	TestCaseInput := make([]string, total)
+	TestCaseOutput := make([]string, total)
+
+	for i := 0; i < total; i++ {
+		TestCaseInput[i] = problem[p].TestCase[i].Input
+		TestCaseOutput[i] = problem[p].SecretCase[i].Output
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"status": "success",
+		"problem":             problem[p].ProblemDescription,
+		"testCaseTotalNumber": total,
+		"testCaseInputData":   TestCaseInput,
+		"testCaseOutputData":  TestCaseOutput,
 	})
 }
