4bb490c0d141f888ce9c8664fb5e9ddb774ed866
Aoi Kondo <agotadmidori@gmail.com>
2024-06-28 19:12:37 +0900

    Update root file
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- frontend/src/components/problem/problem.tsx
+++ frontend/src/components/problem/problem.tsx
@@ -4,15 +4,31 @@ import Description from "./description";
 import Case from "./case";
 import Title from "./title";
 import Appendix from "./appendix";
+import Limitation from "./limitation";
+import { handleGetProblemTotalNumber, handleGetProblemCaseTotalNumber, handleTitle, handleDescription, handleAppendix, handleLimitation, handleCase } from "./hooks";
 
 export default function Problem(){
+    const [nowProblemNumber, SetNowProblem] = React.useState(1)
+    const handleNowProblemChange = (number) => {
+        SetNowProblem(number)
+    }
+    
+    const problemTotalNumber = handleGetProblemTotalNumber()
+    const problemCaseTotalNumber = handleGetProblemCaseTotalNumber(nowProblemNumber)
+    const { problemTitle } = handleTitle(nowProblemNumber)
+    const problemDescription = handleDescription(nowProblemNumber)
+    const { problemAppendix } = handleAppendix(nowProblemNumber)
+    const { problemLimitation} = handleLimitation(nowProblemNumber)
+    const { problemCase } = handleCase(nowProblemNumber, problemTotalNumber)
+
     return (
         <div>
-            <Menu />
-            <Title />
-            <Appendix />
-            <Description />
-            <Case />
+            <Menu problemTotal = { problemTotalNumber } handleNowProblemChange = { handleNowProblemChange }/>
+            <Title problemTitle = { problemTitle }/>
+            <Appendix problemAppendix = { problemAppendix }/>
+            <Description problemDescription = { problemDescription }/>
+            <Limitation problemLimitation = { problemLimitation }/>
+            <Case problemCase = { problemCase } problemCaseTotalNumber = { problemCaseTotalNumber }/>
         </div>
     );
 }
