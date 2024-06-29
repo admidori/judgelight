9ff8ca5da7991fd16bfba6f24792d1442e051a8d
Aoi Kondo <agotadmidori@gmail.com>
2024-06-29 10:32:13 +0900

    Create header component
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- frontend/src/components/common/header/header.tsx
+++ frontend/src/components/common/header/header.tsx
@@ -1,36 +1,12 @@
-import { AppBar, Container, Toolbar, Typography, Box , Button, ButtonGroup } from "@material-ui/core";
-import React from "react";
-import Link from "next/link";
+import React from 'react'
+import Menu from './menu'
+import Login from './login'
 
 export default function Header() {
     return (
-        <AppBar position="static">
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                    <Box sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'space-between',
-                    }}>
-                        <ButtonGroup>
-                            <Link href="/problem">
-                                <Button
-                                    style={{ margin: '10px', color: 'white', display: 'block' , borderColor: 'white'}}
-                                >
-                                    PROBLEMSET
-                                </Button>
-                            </Link>
-                            <Link href="/result">
-                                <Button
-                                    style={{ margin: '10px', color: 'white', display: 'block' , borderColor: 'white'}}
-                                >
-                                    RESULT
-                                </Button>
-                            </Link>
-                        </ButtonGroup>
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar>
+        <div>
+            <Login />
+            <Menu />
+        </div>
     )
 }
