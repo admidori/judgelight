d478205ed818e413770dda6a59fdc71e7868e826
Aoi Kondo <agotadmidori@gmail.com>
2024-06-29 13:19:23 +0900

    Create login auth function
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- frontend/src/provider/auth.tsx
+++ frontend/src/provider/auth.tsx
@@ -8,53 +8,39 @@ type AuthInfo = {
 }
 
 export const LogginContext = React.createContext<boolean>(false)
-export const AuthInfoContext = React.createContext<[AuthInfo, React.Dispatch<React.SetStateAction<AuthInfo>>]>([{ userId: "", userPassword: "" }, () => {}]);
-
-function getLocalStorageAuthInfo(): AuthInfo {
-    const localStorageAuthInfo = localStorage.getItem("authInfo");
-    if (localStorageAuthInfo) {
-        return JSON.parse(localStorageAuthInfo);
-    }else {
-    return { userId: "", userPassword: "" }
-    }
-}
-
-function setLocalStorageAuthInfo(authInfo: AuthInfo) {
-    window.localStorage.setItem("authInfo", JSON.stringify(authInfo));
-}
+export const AuthInfoContext = React.createContext<[AuthInfo, React.Dispatch<React.SetStateAction<AuthInfo>>]>([{ userId: "", userPassword: "" }, () => {}])
 
 export default function AuthContextProvider(props){
-    const [ authInfo, setAuthInfo ] = React.useState<AuthInfo>({ userId: "", userPassword: "" });
-    const [ loginStatus, setLoginStatus ] = React.useState<boolean>(false);
+    const [ authInfo, setAuthInfo ] = React.useState<AuthInfo>({userId: "", userPassword: ""});
+    const [ loginStatus, setLoginStatus ] = React.useState<boolean>(false)
 
     useEffect(() => {
-        if (authInfo?.userId && authInfo?.userPassword) {
-            const sendJsonData = JSON.stringify(
-                {
-                    "student_id": authInfo.userId,
-                    "password": authInfo.userPassword
-                }
-            );
+        if (authInfo?.userId && authInfo?.userPassword && loginStatus == false) {
+            const sendJsonData = JSON.stringify({
+                "student_id": authInfo.userId,
+                "password": authInfo.userPassword
+            })
     
             axios.post(baseURL+'/register/login',sendJsonData)
             .then(function(response){
                 const responseJsonData = JSON.parse(JSON.stringify(response))
                 console.log(responseJsonData.data)
                 if (responseJsonData.data.status == "success"){
+                    setAuthInfo(authInfo)
                     alert("Login Successfull")
+                    localStorage.setItem("authInfo", JSON.stringify(authInfo))
+                    setLoginStatus(true)
                 }
                 else{
                     alert("Login Failed")
+                    setLoginStatus(false)
                 }
             })
             .catch(function(error){
                 console.log(error)
             })
-            setLoginStatus(true)
-        }else{
-            setLoginStatus(false)
         }
-    }, [authInfo]);
+    }, [authInfo])
 
     return(
         <LogginContext.Provider value={loginStatus}>
