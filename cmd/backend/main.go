5c6b0924a0e6478e2c38463b849ecf5153283ed5
Aoi Kondo <agotadmidori@gmail.com>
2024-06-29 19:45:17 +0900

    Update API define
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -1,19 +1,11 @@
 /*
-Copyright © 2023 Aoi Kondo <agotadmidori@gmail.com>
-*/
-
-/*
-Function: This is backend program for judgelight.
-		  Receive "Submit datafile" from axios on React(Next.js).
-		  And send a json file to judge-language server.
+Copyright © 2024 Aoi Kondo <agotadmidori@gmail.com>
 */
 
 package main
 
 import (
-	"github.com/admidori/judgelight/pkg/controller/database"
-	"github.com/admidori/judgelight/pkg/controller/paramater"
-	"github.com/admidori/judgelight/pkg/controller/problem"
+	"github.com/admidori/judgelight/pkg/controller/api"
 	"github.com/admidori/judgelight/pkg/controller/submit"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -22,13 +14,11 @@ import (
 func main() {
 	router := gin.Default()
 	router.Use(cors.Default())
-	// If submit programs, excute below.
+	
 	router.POST("/program/submit", submit.ReceiveSubmitProgram)
-	router.GET("/paramater", paramater.SendParamater)
-	router.GET("/problem", problem.SendProblem)
-
-	// If register user, excute below.
-	router.POST("/register/problem", database.RegisterProblem)
+	router.POST("/register/login", api.Login)
+	router.GET("/get/problem/info", api.GetProblemInfo)
+	router.GET("/get/contest/info", api.GetContestInfo)
 
 	// Start listening.
 	router.Run(":8080")
