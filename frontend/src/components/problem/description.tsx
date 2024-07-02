3f859539581a7aae4d6d6d7ab6adb3743af2d46b
Aoi Kondo <agotadmidori@gmail.com>
2024-07-03 08:11:42 +0900

    Update layout
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- frontend/src/components/problem/description.tsx
+++ frontend/src/components/problem/description.tsx
@@ -4,7 +4,15 @@ import { AppBar, Box, Container, Toolbar } from "@material-ui/core";
 export default function Description(props){
     return (
         <Container maxWidth="md">
-            <div style={{border: "#000000", borderStyle: "solid", borderWidth: "1px", fontSize: "100%", padding: "20px"}}>{props.problemDescription}</div>
+            <p style={{
+                whiteSpace: "pre-wrap",
+                wordWrap: "break-word",
+                border: "#000000",
+                borderStyle: "solid",
+                borderWidth: "1px",
+                fontSize: "100%",
+                padding: "20px"
+                }}>{props.problemDescription}</p>
         </Container>
     );
 }
