213f1259daf9486c5632bf5b46a1db3758757b29
Aoi Kondo <agotadmidori@gmail.com>
2024-07-02 12:39:34 +0900

    Create files for testing
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

---
+++ test/register/generate.py
@@ -0,0 +1,27 @@
+import csv
+import random
+import string
+
+def generate_random_string(length):
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for _ in range(length))
+
+def generate_test_data(num_users):
+    data = []
+    for _ in range(num_users):
+        username = generate_random_string(5)
+        password = generate_random_string(10)
+        data.append([username, password])
+    return data
+
+def write_csv_file(file_path, data):
+    with open(file_path, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
+
+# Generate test data for 1000 users
+test_data = generate_test_data(1000)
+
+# Write the data to a CSV file
+file_path = '/home/admidori/judgelight/test/register/test.csv'
+write_csv_file(file_path, test_data)
