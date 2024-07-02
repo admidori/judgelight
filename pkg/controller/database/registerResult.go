0b95aae06435d9dcb401591a1af9715775f397ff
Aoi Kondo <agotadmidori@gmail.com>
2024-07-02 10:01:36 +0900

    Refactoring
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- pkg/controller/database/registerResult.go
+++ pkg/controller/database/registerResult.go
@@ -6,12 +6,13 @@ import (
 
 func RegisterResult(res Result_table) {
 	db := InitDatabase()
-	
+
 	stmt, err := db.Prepare(`INSERT INTO JUDGELIGHT.RESULT (STUDENT_ID, PROBLEM_ID, RESULT) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE RESULT = VALUES(RESULT)`)
 	if err != nil {
 		panic(err)
 	}
-	result, err := stmt.Exec(res.Student_id, res.Problem_num+1, res.Result)
+
+	result, err := stmt.Exec(res.Student_id, res.Problem_num, res.Result)
 	if err != nil {
 		panic(err)
 	}
