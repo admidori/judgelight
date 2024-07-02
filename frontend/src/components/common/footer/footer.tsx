bc0fd7fd0268c130883e42929f37f359ebee5b85
Aoi Kondo <agotadmidori@gmail.com>
2024-07-02 13:07:49 +0900

    Major-Update: Change the version and code-name
    Signed-off-by: Aoi Kondo <agotadmidori@gmail.com>

--- frontend/src/components/common/footer/footer.tsx
+++ frontend/src/components/common/footer/footer.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Config from "../../../../config.json";
 
 export default function Footer(){
     return (
         <footer style={{ height: 20, background: "#666", color: "#fff" }}>
-            <p style={{ textAlign: "center" }}>Judgelight v1.0 (Beta)</p>
+            <p style={{ textAlign: "center" }}>Judgelight v{Config.version} ({Config.codeName})</p>
         </footer>
     )
 }
--- frontend/src/types/config.d.tsx
+++ frontend/src/types/config.d.tsx
@@ -2,6 +2,8 @@ declare module 'config.json' {
     interface Config {
         domain: string;
         apiPort: number;
+        version: string;
+        codename: string;
     }
 
     const value: Config;
