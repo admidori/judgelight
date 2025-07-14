d6e558aa48002730a537180898835a668f5ddd37
Aoi Kondo <aoi.kondo@admidori.net>
2025-07-14 16:07:07 +0900

    Update footer layout

--- frontend/src/components/common/footer/footer.tsx
+++ frontend/src/components/common/footer/footer.tsx
@@ -3,7 +3,7 @@ import Config from "../../../../config.json";
 
 export default function Footer(){
     return (
-        <footer style={{ height: 20, background: "#666", color: "#fff" }}>
+        <footer style={{ height: 25, background: "#666", color: "#fff" }}>
             <p style={{ textAlign: "center" }}>Judgelight v{Config.version} ({Config.codeName})</p>
         </footer>
     )
