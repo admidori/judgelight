6c8fcdf1942db0586f5c7ce5c5234b457a9d4548
Aoi Kondo <aoi.kondo@admidori.net>
2025-11-29 16:53:02 +0900

    Fix the import bug

--- frontend/src/pages/result.tsx
+++ frontend/src/pages/result.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import Header from "../components/common/header/header";
-import Footer from '../components/common/footer/footer';
 import Database from "../components/result/database";
 import AuthContextProvider from "../provider/auth";
 import Score from "../components/result/score";
@@ -13,7 +12,6 @@ export default function Result() {
             <Header />
             <Score />
                 <Database />
-                <Footer />
             </AuthContextProvider>
         </div>
     )
