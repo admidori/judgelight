4d3cb29aab6f2d6c1e92fcd68f99e97504af426c
Aoi Kondo <aoi.kondo@admidori.net>
2025-11-29 17:18:12 +0900

    Merge components

--- frontend/src/pages/problem.tsx
+++ frontend/src/pages/problem.tsx
@@ -1,48 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Layout } from 'antd';
 import Header from "../components/common/header/header";
-import GeneralMenu from '../components/common/menu/generalMenu';
 import ProgramEditor from '../components/editor/editor';
 import Problem from '../components/problem/problem';
 import AuthContextProvider from '../provider/auth';
 import ProblemNumberProvider from '../provider/problemNumber';
+import Menu from '../components/common/menu/menu';
 
 const { Sider, Content } = Layout;
 
 export default function ProblemPage() {
-    const [collapsed, setCollapsed] = useState(false);
-
     return (
         <ProblemNumberProvider>
             <AuthContextProvider>
                 <Layout style={{ minHeight: '100vh' }}>
                     <Header />
                     <Layout>
-                        <Sider 
-                            width={256}
-                            collapsedWidth={80}
-                            theme="light"
-                            style={{ 
-                                background: '#fff',
-                                borderRight: '1px solid #f0f0f0'
-                            }}
-                            trigger={null}
-                            collapsible
-                            collapsed={collapsed}
-                        >
-                            <GeneralMenu collapsed={collapsed} onCollapse={setCollapsed} />
-                        </Sider>
-                        <Layout>
-                            <Content style={{ 
-                                padding: '24px',
-                                background: '#fff',
-                                minHeight: 280
-                            }}>
-                                <Problem />
-                                <hr />
-                                <ProgramEditor />
-                            </Content>
-                        </Layout>
+                        <Menu />
+                        <Content style={{ 
+                            padding: '24px',
+                            background: '#fff',
+                            minHeight: 280
+                        }}>
+                            <Problem />
+                            <hr />
+                            <ProgramEditor />
+                        </Content>
                     </Layout>
                 </Layout>
             </AuthContextProvider>
--- frontend/src/pages/result.tsx
+++ frontend/src/pages/result.tsx
@@ -1,18 +1,26 @@
 import React from "react";
-
 import Header from "../components/common/header/header";
 import Database from "../components/result/database";
 import AuthContextProvider from "../provider/auth";
 import Score from "../components/result/score";
+import Menu from "../components/common/menu/menu";
+import { Layout } from "antd";
+
+const { Sider, Content } = Layout;
 
 export default function Result() {
     return(
-        <div>
-            <AuthContextProvider>
-            <Header />
-            <Score />
-                <Database />
-            </AuthContextProvider>
-        </div>
+        <AuthContextProvider>
+            <Layout style={{ minHeight: '100vh' }}>
+                <Header />
+                <Layout>
+                    <Menu />
+                    <Content>
+                        <Score />
+                        <Database />
+                    </Content>
+                </Layout>
+            </Layout>
+        </AuthContextProvider>
     )
 }
